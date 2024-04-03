d48f2fb3bf8005b28ba0b5fc7b374b82f0d309ed
Benzoin96485 <luowl7@mit.edu>
2024-04-03 13:51:24 -0400

    finish nn translation

--- mlff/models/physnet/nn.py
+++ mlff/models/physnet/nn.py
@@ -131,73 +131,69 @@ class PhysNet(nn.Module):
 
     #calculates the energy and force given the atomic properties (in order to prevent recomputation if atomic properties are calculated)
     def energy_and_forces_from_atomic_properties(self, Ea, Qa, Dij, Z, R, idx_i, idx_j, Q_tot=None, batch_seg=None):
-        with tf.name_scope("energy_and_forces_from_atomic_properties"):
-            energy = self.energy_from_atomic_properties(Ea, Qa, Dij, Z, idx_i, idx_j, Q_tot, batch_seg)
-            forces = -tf.convert_to_tensor(tf.gradients(tf.reduce_sum(energy), R)[0])
+        energy = self.energy_from_atomic_properties(Ea, Qa, Dij, Z, idx_i, idx_j, Q_tot, batch_seg)
+        forces = -torch.autograd.grad(torch.sum(energy), R)[0]
         return energy, forces
 
     #calculates the total energy (including electrostatic interactions)
     def energy(self, Z, R, idx_i, idx_j, Q_tot=None, batch_seg=None, offsets=None, sr_idx_i=None, sr_idx_j=None, sr_offsets=None):
-        with tf.name_scope("energy"):
-            Ea, Qa, Dij, _ = self.atomic_properties(Z, R, idx_i, idx_j, offsets, sr_idx_i, sr_idx_j, sr_offsets)
-            energy = self.energy_from_atomic_properties(Ea, Qa, Dij, Z, idx_i, idx_j, Q_tot, batch_seg)
+        Ea, Qa, Dij, _ = self.atomic_properties(Z, R, idx_i, idx_j, offsets, sr_idx_i, sr_idx_j, sr_offsets)
+        energy = self.energy_from_atomic_properties(Ea, Qa, Dij, Z, idx_i, idx_j, Q_tot, batch_seg)
         return energy 
 
     #calculates the total energy and forces (including electrostatic interactions)
     def energy_and_forces(self, Z, R, idx_i, idx_j, Q_tot=None, batch_seg=None, offsets=None, sr_idx_i=None, sr_idx_j=None, sr_offsets=None):
-        with tf.name_scope("energy_and_forces"):
-            Ea, Qa, Dij, _ = self.atomic_properties(Z, R, idx_i, idx_j, offsets, sr_idx_i, sr_idx_j, sr_offsets)
-            energy, forces = self.energy_and_forces_from_atomic_properties(Ea, Qa, Dij, Z, R, idx_i, idx_j, Q_tot, batch_seg)
+        Ea, Qa, Dij, _ = self.atomic_properties(Z, R, idx_i, idx_j, offsets, sr_idx_i, sr_idx_j, sr_offsets)
+        energy, forces = self.energy_and_forces_from_atomic_properties(Ea, Qa, Dij, Z, R, idx_i, idx_j, Q_tot, batch_seg)
         return energy, forces
 
     #returns scaled charges such that the sum of the partial atomic charges equals Q_tot (defaults to 0)
     def scaled_charges(self, Z, Qa, Q_tot=None, batch_seg=None):
-        with tf.name_scope("scaled_charges"):
-            if batch_seg is None:
-                batch_seg = tf.zeros_like(Z)
-            #number of atoms per batch (needed for charge scaling)
-            Na_per_batch = tf.segment_sum(tf.ones_like(batch_seg, dtype=self.dtype), batch_seg)
-            if Q_tot is None: #assume desired total charge zero if not given
-                Q_tot = tf.zeros_like(Na_per_batch, dtype=self.dtype)
-            #return scaled charges (such that they have the desired total charge)
-            return Qa + tf.gather(((Q_tot-tf.segment_sum(Qa, batch_seg))/Na_per_batch), batch_seg)
+        if batch_seg is None:
+            batch_seg = torch.zeros_like(Z)
+        #number of atoms per batch (needed for charge scaling)
+        Na_per_batch = segment_sum(torch.ones_like(batch_seg, dtype=self.dtype), batch_seg)
+        if Q_tot is None: #assume desired total charge zero if not given
+            Q_tot = torch.zeros_like(Na_per_batch, dtype=self.dtype)
+        #return scaled charges (such that they have the desired total charge)
+        return Qa + ((Q_tot - segment_sum(Qa, batch_seg)) / Na_per_batch)[batch_seg]
 
     #switch function for electrostatic interaction (switches between shielded and unshielded electrostatic interaction)
     def _switch(self, Dij):
-        cut = self.sr_cut/2
-        x  = Dij/cut
-        x3 = x*x*x
-        x4 = x3*x
-        x5 = x4*x
-        return tf.where(Dij < cut, 6*x5-15*x4+10*x3, tf.ones_like(Dij))
+        cut = self.sr_cut / 2
+        x  = Dij / cut
+        x3 = x ** 3
+        x4 = x3 * x
+        x5 = x4 * x
+        return torch.where(Dij < cut, 6 * x5 - 15 * x4 + 10 * x3, torch.ones_like(Dij))
 
     #calculates the electrostatic energy per atom 
     #for very small distances, the 1/r law is shielded to avoid singularities
     def electrostatic_energy_per_atom(self, Dij, Qa, idx_i, idx_j):
         #gather charges
-        Qi = tf.gather(Qa, idx_i)
-        Qj = tf.gather(Qa, idx_j)
+        Qi = Qa[idx_i]
+        Qj = Qa[idx_j]
         #calculate variants of Dij which we need to calculate
         #the various shileded/non-shielded potentials
-        DijS = tf.sqrt(Dij*Dij + 1.0) #shielded distance
+        DijS = torch.sqrt(Dij * Dij + 1.0) #shielded distance
         #calculate value of switching function
         switch = self._switch(Dij) #normal switch
-        cswitch = 1.0-switch #complementary switch
+        cswitch = 1.0 - switch #complementary switch
         #calculate shielded/non-shielded potentials
         if self.lr_cut is None: #no non-bonded cutoff
-            Eele_ordinary = 1.0/Dij   #ordinary electrostatic energy
-            Eele_shielded = 1.0/DijS  #shielded electrostatic energy
+            Eele_ordinary = 1.0 / Dij   #ordinary electrostatic energy
+            Eele_shielded = 1.0 / DijS  #shielded electrostatic energy
             #combine shielded and ordinary interactions and apply prefactors 
-            Eele = self.kehalf*Qi*Qj*(cswitch*Eele_shielded + switch*Eele_ordinary)
+            Eele = self.kehalf * Qi * Qj * (cswitch * Eele_shielded + switch * Eele_ordinary)
         else: #with non-bonded cutoff
-            cut   = self.lr_cut
-            cut2  = self.lr_cut*self.lr_cut
-            Eele_ordinary = 1.0/Dij  +  Dij/cut2 - 2.0/cut
-            Eele_shielded = 1.0/DijS + DijS/cut2 - 2.0/cut
+            cut = self.lr_cut
+            cut2 = self.lr_cut * self.lr_cut
+            Eele_ordinary = 1.0 / Dij + Dij / cut2 - 2.0 / cut
+            Eele_shielded = 1.0 / DijS + DijS / cut2 - 2.0 / cut
             #combine shielded and ordinary interactions and apply prefactors 
-            Eele = self.kehalf*Qi*Qj*(cswitch*Eele_shielded + switch*Eele_ordinary)
-            Eele = tf.where(Dij <= cut, Eele, tf.zeros_like(Eele))
-        return tf.segment_sum(Eele, idx_i)
+            Eele = self.kehalf * Qi * Qj * (cswitch * Eele_shielded + switch * Eele_ordinary)
+            Eele = torch.where(Dij <= cut, Eele, torch.zeros_like(Eele))
+        return segment_sum(Eele, idx_i)
 
 
     @property
