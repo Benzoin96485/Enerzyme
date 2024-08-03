fix dense layer precision problem

enerzyme/models/layers/electrostatics.py
@@ -1,4 +1,4 @@
-from typing import Dict, Literal
+from typing import Dict, Literal, Optional
 import torch
 from torch import nn, Tensor
 from ..functional import segment_sum
@@ -20,9 +20,9 @@ class ChargeConservationLayer(nn.Module):
 
     def get_corrected_Qa(
         self, 
-        Za: torch.Tensor, Qa: torch.Tensor, 
-        Q: torch.Tensor=None, batch_seg: torch.Tensor=None, **kwargs
-    ) -> Dict[Literal["Qa", "Q"], torch.Tensor]:
+        Za: Tensor, Qa: Tensor, 
+        Q: Optional[Tensor]=None, batch_seg: Optional[Tensor]=None, **kwargs
+    ) -> Dict[Literal["Qa", "Q"], Tensor]:
         '''
         Correct the atomic charge
 

enerzyme/models/layers/mlp.py
@@ -43,37 +43,34 @@ class DenseLayer(NeuronLayer):
     ):
         super().__init__(dim_feature_in, dim_feature_out, activation_fn, activation_params)
 
-        self.linear = nn.Linear(dim_feature_in, dim_feature_out, use_bias)
         if initial_weight == "semi_orthogonal_glorot":
-            with torch.no_grad():
-                self.linear.weight.data.copy_(semi_orthogonal_glorot_weights(dim_feature_in, dim_feature_out))
+            self.weight = nn.Parameter(semi_orthogonal_glorot_weights(dim_feature_in, dim_feature_out))
         elif initial_weight == "orthogonal":
-            init.orthogonal_(self.linear.weight.data)
+            self.weight = nn.Parameter(torch.empty(dim_feature_out, dim_feature_in))
+            init.orthogonal_(self.weight)
         elif initial_weight == "zero":
-            init.zeros_(self.linear.weight.data)
+            self.weight = nn.Parameter(torch.empty(dim_feature_out, dim_feature_in))
+            init.zeros_(self.weight)
         else:
-            with torch.no_grad():
-                self.linear.weight.data.copy_(torch.tensor(initial_weight))
+            self.weight = nn.Parameter(torch.tensor(initial_weight))
         if use_bias:
             if initial_bias == "zero":
-                nn.init.zeros_(self.linear.bias)
+                self.bias = nn.Parameter(torch.empty(dim_feature_out))
+                init.zeros_(self.bias)
             else:
-                self.linear.bias.data.copy_(torch.tensor(initial_bias))     
-        
-        if activation_fn is None:
-            self.op = self.linear
+                self.bias = nn.Parameter(torch.tensor(initial_bias))
         else:
-            self.op = nn.Sequential(self.linear, self.activation_fn)
+            self.bias = None 
 
     def forward(self, x: Tensor) -> Tensor:
-        return self.op(x)
-    
-    @property
-    def W(self) -> Tensor:
-        return self.linear.weight.data
+        y = F.linear(x, self.weight, self.bias)
+        if self.activation_fn is None:
+            return y
+        else:
+            return self.activation_fn(y)
 
     def l2loss(self) -> Tensor:
-        return F.mse_loss(self.W, torch.zeros_like(self.W), reduction="sum") / 2
+        return F.mse_loss(self.weight, torch.zeros_like(self.weight), reduction="sum") / 2
     
 
 class ResidualLayer(NeuronLayer):

test/test_physnet.py
@@ -37,7 +37,7 @@ idx_i = idx_i.reshape(-1)
 idx_j = idx_j.reshape(-1)
 offsets = np.random.random((*idx_i.shape, 3))
 x = np.random.randn(N, F)
-W_init = semi_orthogonal_glorot_weights(F, F)
+W_init = semi_orthogonal_glorot_weights(F, F) * 10000
 b_init = np.random.randn(F)
 D = np.random.random(*idx_i.shape) * 30 + 3
 rbf = np.random.randn(*idx_i.shape, K)
@@ -183,8 +183,8 @@ def test_DenseLayer():
         sess.run(tf.global_variables_initializer())
         y_dense_tf = dense_layer_tf(x).eval()
         l2loss_tf = dense_layer_tf.l2loss.eval()
-    assert_allclose(y_dense_torch, y_dense_tf, rtol=1e-6, atol=1e-6)
-    assert_allclose(l2loss_torch, l2loss_tf, rtol=1e-6, atol=1e-6)
+    assert_allclose(y_dense_torch, y_dense_tf)
+    assert_allclose(l2loss_torch, l2loss_tf)
 
 
 def test_ResidualLayer():
@@ -197,7 +197,7 @@ def test_ResidualLayer():
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         y_residual_tf = residual_layer_tf(x.copy()).eval()
-    assert_allclose(y_residual_torch, y_residual_tf, rtol=1e-6, atol=1e-6)
+    assert_allclose(y_residual_torch, y_residual_tf)
 
 
 def test_InteractionLayer():
@@ -216,7 +216,7 @@ def test_InteractionLayer():
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         y_interaction_tf = interaction_layer_tf(x.copy(), rbf.copy(), idx_i, idx_j).eval()
-    assert_allclose(y_interaction_torch, y_interaction_tf, rtol=1e-6, atol=1e-6)
+    assert_allclose(y_interaction_torch, y_interaction_tf)
 
 
 def test_InteractionBlock():
@@ -235,7 +235,7 @@ def test_InteractionBlock():
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         y_interaction_tf = interaction_block_tf(x.copy(), rbf.copy(), idx_i, idx_j).eval()
-    assert_allclose(y_interaction_torch, y_interaction_tf, rtol=1e-5, atol=1e-5)
+    assert_allclose(y_interaction_torch, y_interaction_tf)
 
 
 def test_OutputBlock():
@@ -278,7 +278,7 @@ def test_atomic_properties():
         nhloss_tf = nhloss_tf.eval()
     assert_allclose(Ea_torch, Ea_tf)
     assert_allclose(Qa_torch, Qa_tf)
-    assert_allclose(Dij_lr_torch, Dij_lr_tf, rtol=1e-6, atol=1e-6)
+    assert_allclose(Dij_lr_torch, Dij_lr_tf)
     assert_allclose(nhloss_torch, nhloss_tf)
 
 